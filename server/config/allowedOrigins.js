f673c53c9539b9d60f3792a3d4078cf6ea83d90c
Bolu Yomi-olugbodi  (Anime Code Lord) <98047628+yommexg@users.noreply.github.com>
2023-10-31 20:46:56 +0100

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  "https://teedeeposh-git-main-yommexg.vercel.app",
-  "https://teedeeposh-hz57.vercel.app",
+  "https://teedeeposh-git-main-yommexg.vercel.app/",
+  "https://teedeeposh-hz57.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
