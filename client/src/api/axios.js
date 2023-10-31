d92b53a213849580db0300e7c429553370f1c97a
Yomi-Olugbodi Boluwatife <animecodelord@gmail.com>
2023-10-31 21:14:13 +0100

    Update allowed origin and clientapi

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://posh-server-api.onrender.com";
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+  "https://teedeeposh-git-main-yommexg.vercel.app/",
   "https://teedeeposh-hz57.vercel.app/",
 ];
 
