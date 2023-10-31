c7918cf517769980424dd16a4e5df3ac691b6e1b
Yomi-Olugbodi Boluwatife <animecodelord@gmail.com>
2023-10-31 17:49:55 +0100

    Updated allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,10 +1,6 @@
 const allowedOrigins = [
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "https://teedee-posh-admin-yommexg.vercel.app",
-  "https://teedee-posh-h76gz4ucb-yommexg.vercel.app",
+  "https://teedeeposh-git-main-yommexg.vercel.app/",
+  "https://teedeeposh-hz57.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
